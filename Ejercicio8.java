790785aef2038a1289b10b5cf3356bc2f90fe51b
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-11 09:10:56 +0200

    feat: Ejercicio8

---
+++ Ejercicio8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/**
+ * Exercise: 8
+ * 
+ * @version: 11/10/2022 08:55
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float horas;
+        System.out.println("Introduce las horas trabajadas:");
+        horas = sc.nextFloat();
+        System.out.println(
+                "El salario para " + horas + " horas trabajadas sería de " + String.format("%.2f", horas * 12)
+                        + " euros");
+        sc.close();
+    }
+}

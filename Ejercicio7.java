86b554c2be570bd2510a09ff383d04b4e82d00fe
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-11 08:54:21 +0200

    feat: Ejercicio7

---
+++ Ejercicio7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Exercise: 7
+ * 
+ * @version: 11/10/2022 08:46
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float base;
+        System.out.println("Introduce la base de la factura:");
+        base = sc.nextFloat();
+        System.out.println("Con una base de "
+                + String.format("%.2f", base) + " euros y un IVA del 21% ("
+                + String.format("%.2f", (base * .21))
+                + " euros) hace un total factura de: "
+                + String.format("%.2f", (base * 1.21)) + " euros");
+        sc.close();
+    }
+}

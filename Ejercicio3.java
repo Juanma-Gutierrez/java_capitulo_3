bc27c7e9772305c11182affb90711be93e65e527
Juanma-Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2022-10-11 08:28:45 +0200

    feat: Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+/**
+ * Exercise: 3
+ * 
+ * @version: 11/10/2022 08:23
+ * @author: Juan Manuel Gutiérrez
+ *          https://github.com/Juanma-Gutierrez
+ */
+
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pesetas;
+        System.out.println("Introduce la cantidad de pesetas:");
+        pesetas = sc.nextInt();
+        System.out.println(pesetas + " pesetas equivalen a " + String.format("%.2f",(pesetas / 166.386)) + " euros");
+        sc.close();
+    }
+}
